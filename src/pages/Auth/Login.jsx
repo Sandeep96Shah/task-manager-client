670d3b4ee85969377916bb6b9551fea324f49798
Sandeep Shah <sandeep2016shah@gmail.com>
2025-06-07 19:22:11 +0600

    check

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   // handle login form submit
   const handleLogin = (event) => {
