9d6af91e756d900e0c24c2366bce2f686541b225
Sandeep Shah <sandeep2016shah@gmail.com>
2025-06-10 15:22:21 +0600

    implemented table for recent tasks in dashboard

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -1,15 +1,109 @@
-import React, { useContext } from 'react'
-import DashboardLayout from '../../components/layouts/DashboardLayout'
-import { UserContext } from '../../context/userContext'
-import { useUserAuth } from '../../hooks/useUserAuth'
+import { rules } from "eslint-plugin-react-refresh";
+import moment from "moment";
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
+import { LuArrowRight } from "react-icons/lu";
+import { useNavigate } from "react-router-dom";
+import InfoCard from "../../components/Cards/InfoCard";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import TaskListTable from "../../components/TaskListTable";
+import { UserContext } from "../../context/userContext";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { API_PATHS } from "../../utils/apiPaths";
+import axiosInstance from "../../utils/axiosInstance";
+import { addThousandsSeparator } from "../../utils/helper";
 
 const Dashboard = () => {
-  useUserAuth()
+  useUserAuth();
 
   const { user } = useContext(UserContext);
-  return (
-    <DashboardLayout activeMenu={"Dashboard"}>Dashboard</DashboardLayout>
-  )
-}
+  const navigate = useNavigate();
 
-export default Dashboard
+  const [dashboardData, setDashboardData] = useState(null);
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log(`Error while fetching, ${error.message}`);
+    }
+  };
+
+  const handleOnSeeMore = () => {
+    navigate("/admin/tasks")
+  };
+
+  useEffect(() => {
+    getDashboardData();
+
+    return () => {};
+  }, []);
+  return (
+    <DashboardLayout activeMenu={"Dashboard"}>
+      <div className="card my-5">
+        <div>
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">Good Morning ! {user?.name}</h2>
+            <p className="text-xs md:text[13px] text-gray-400 mt-1.5">
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+          <InfoCard
+            label="Total Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.All || 0
+            )}
+            color="bg-primary"
+          />
+
+          <InfoCard
+            label="Pending Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Pending || 0
+            )}
+            color="bg-violet-500"
+          />
+
+          <InfoCard
+            label="In Progress Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.InProgress || 0
+            )}
+            color="bg-cyan-500"
+          />
+
+          <InfoCard
+            label="Completed Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Completed || 0
+            )}
+            color="bg-lime-500"
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+          <div className="md:col-span-2">
+            <div className="card">
+              <div className="flex items-center justify-between">
+                <h5 className="text-lg"> Recent Tasks </h5>
+                <button className="card-btn" onClick={handleOnSeeMore}>
+                  See All <LuArrowRight className="text-base" />
+                </button>
+              </div>
+              <TaskListTable tableData={dashboardData?.recentTasks || []} />
+            </div>
+          </div>
+        </div>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
